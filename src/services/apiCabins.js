1c872d7237d0aeb0950d0a1cc74ea90fdc7304e4
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-11 22:20:51 +0700

    009 Setting Up Storage Buckets

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,7 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  let { data, error } = await supabase.from("guests").select("*");
+  let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error(error);
