79a9fbb00f90826f6578ebb1282866acf5f6fae7
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-24 16:27:04 +0700

    009 API-Side Pagination Paginating Bookings

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -7,7 +7,7 @@ import Spinner from "../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, count, isLoading } = useBookings();
 
   if (isLoading) return <Spinner />;
   if (!bookings) return <Empty resourceName="bookings" />;
@@ -32,7 +32,7 @@ function BookingTable() {
         />
 
         <Table.Footer>
-          <Pagination count={5} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
