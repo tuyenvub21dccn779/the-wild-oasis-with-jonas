1ef2ba9563751c3c24d6f252883f5fa7d5ade9ea
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-05-25 22:18:06 +0700

    018 User Logout

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -18,6 +18,10 @@ export async function getCurrentUser() {
   const { data, error } = await supabase.auth.getUser();
 
   if (error) throw new Error(error.message);
-
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
